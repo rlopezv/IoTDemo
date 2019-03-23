667ec88eee5090dd19f3d5e0671fe85884cc5a6a
rlopezv <ramon.lopez@ezentis.com>
2019-03-23 12:44:58 +0100

    Implementation

--- IoTHubDemoApp/Controllers/HomeController.cs
+++ IoTHubDemoApp/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using IoTHubDemoApp.Hubs;
+using Microsoft.AspNet.SignalR;
+using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,22 +14,39 @@ namespace IoTHubDemoApp.Controllers
 {
     public class HomeController : Controller
     {
+        private string connectionString = "Endpoint=sb://servicebusiothub.servicebus.windows.net/;SharedAccessKeyName=iothubroutes_IoTHUB-rg;SharedAccessKey=Ti1kdbMMT76pXCRKZRb5AdG6g9mYmFrwMQp1ZrDEUh4=";
+        private string queueName = "iothubqueue";
+        private IHubContext _hubContext;
+
+        public HomeController()
+        {
+            _hubContext = GlobalHost.ConnectionManager.GetHubContext<IoTHub>();
+        }
+
         public ActionResult Index()
         {
+            Task task = Task.Run(() =>
+            {
+
+                QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName, ReceiveMode.ReceiveAndDelete);
+
+                client.OnMessage(message =>
+                {
+                    Stream stream = message.GetBody<Stream>();
+                    StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                    string s = reader.ReadToEnd();
+                    _hubContext.Clients.All.ioTHubNotification(s);
+
+                });
+            });
+
+            task.Wait();
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ IoTHubDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(IoTHubDemoApp.Startup))]
+namespace IoTHubDemoApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
